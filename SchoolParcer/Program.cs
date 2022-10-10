68a1720a268e38e0691415c7b9973f8e6d3bdc52
Mixail <mixail.korchikov@mail.ru>
2022-10-10 20:14:41 +0300

    init

---
+++ SchoolParcer/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Schedule;
+
+namespace SchoolParcer
+{
+    internal class Program
+    {
+        internal static HttpClient _client = new HttpClient();
+        internal const string _url = "https://sch2072v.mskobr.ru/uchashimsya/raspisanie-kanikuly";
+        internal const string _schoolName = "Расписание уроков обучающихся учебного корпуса № 3";
+        static void Main(string[] args)
+        {
+            _client.BaseAddress = new Uri(_url);
+            string httpResult = GetHttpAsync().Result;
+            Debug.WriteLine("Start Parse");
+            Parse();
+            Debug.WriteLine("End Parse");
+            Console.ReadKey();
+        }
+
+        private static async Task<string> GetHttpAsync()
+        {
+            HttpResponseMessage response = await _client.GetAsync(_url);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            return null;
+        }
+
+        private static void Parse(string school = _schoolName, string className = "5«К»")
+        {
+            string httpResult = GetHttpAsync().Result;
+            httpResult = WebUtility.HtmlDecode(httpResult);
+            var startTableIndex = httpResult.IndexOf(school);
+            var endTableIndex = httpResult.IndexOf("</table>", startTableIndex);
+
+            List<Row> rows = GetRows(httpResult, startTableIndex, endTableIndex);
+
+            //int count = 0;
+            //foreach (Row row in rows)
+            //{
+            //    if (row.ContainsClassName(httpResult))
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine($"Rows with class name = {count};");  //Class count
+            //Console.WriteLine($"Rows Count = {rows.Count};");
+
+            string currentClassName = string.Empty;
+            foreach (Row row in rows)
+            {
+                if(row.ContainsClassName(httpResult, out string newClassName))
+                {
+                    currentClassName = newClassName;
+                    Console.WriteLine(currentClassName);
+                    //Lesson lesson = new Lesson
+                    //{
+                    //    lessonName = "",
+                    //    className = currentClassName,
+                    //    classRoomNumbers = new List<int> { 1, 2 }
+                    //};
+                }
+            }
+        }
+
+        private static List<Row> GetRows(string html, int start, int end)
+        {
+            var rows = GetAllRows(html);
+            var temp = new List<Row>();
+
+            for (int i = 1; i < rows.Count; i++)
+            {
+                if (rows[i].startIndex >= start && rows[i].endIndex <= end )
+                {
+                    temp.Add(rows[i]);
+                }
+            }
+
+            if (temp.Count > 0) { temp.RemoveAt(0); }   //Удяляем первый элемент, т.к. в нем только дни недели.
+            return temp;
+        }
+
+        private static List<Row> GetAllRows(string html)
+        {
+            List<Row> rows = new List<Row>();
+            if (string.IsNullOrEmpty(html))
+            {
+                return null;
+            }
+
+            int currIndex = 0;
+            Row temp = new Row();
+
+            do
+            {
+                if (!temp.IsZero())
+                {
+                    rows.Add(temp);
+                }
+
+                try
+                {
+                    temp.startIndex = html.IndexOf("<tr>", currIndex);
+                    temp.endIndex = html.IndexOf("</tr>", temp.startIndex);
+                    currIndex = temp.endIndex;
+                }
+                catch
+                {
+                    break;
+                }
+            }
+            while (temp.startIndex != -1);
+
+            return rows;
+        }
+    }
+
+
+}
---
+++ SchoolParcer/Schedule.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Schedule
+{
+    public enum Day
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+    }
+
+    public struct Lesson
+    {
+        public Day Day { get; set; }
+        public string className { get; set; }
+        public string lessonName { get; set; }
+        public string classRoomNumber { get; set; }
+    }
+
+    public class Schedule
+    {
+        public static Dictionary<Day, List<Lesson>> schoolSchedule;
+    }
+
+    public struct Row
+    {
+        public int startIndex { get; set; }
+        public int endIndex { get; set; }
+        public int lenght => endIndex - startIndex;
+
+        public void Clear()
+        {
+            startIndex = -1;
+            endIndex = -1;
+        }
+
+        public bool IsZero()
+        {
+            if (startIndex == 0 && endIndex == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool ContainsClassName(string html, out string className)
+        {
+            className = null;
+            int index = -1;
+
+            try
+            {
+                index = html.IndexOf("<strong>", startIndex);
+            }
+            catch
+            {
+                ;
+            }
+
+            if (index == -1 || index > endIndex)
+            {
+                return false;
+            }
+
+            index += 8;
+            int endNameIndex = html.IndexOf("</strong>", index);
+            className = html.Substring(index, endNameIndex - index);
+            className = className.Replace(" ", String.Empty);
+            className = className.Replace("»", string.Empty);
+            className = className.Replace("«", string.Empty);
+
+            return true;
+        }
+
+        public bool ContainsClassName(string html)
+        {
+            int index = -1;
+            try
+            {
+                index = html.IndexOf("<strong>", startIndex);
+            }
+            catch
+            {
+                ;
+            }
+
+            if (index == -1 || index > endIndex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public List<Lesson> GetLessons(string className, string html)
+        {
+            var lessons = new List<Lesson>();
+            int tryCount = ContainsClassName(html) ? 6 : 5;
+            Lesson lesson = new Lesson();
+            lesson.className = className;
+            Cell temp = new Cell();
+
+            var current
+            do
+            {
+                temp.startIndex = html.IndexOf("<td>", currIndex);
+                temp.endIndex = html.IndexOf("</td>", temp.startIndex);
+                currIndex = temp.endIndex;
+            }
+            while (true);
+        }
+    }
+
+    public struct Cell
+    {
+        public int startIndex {  get; set; }
+        public int endIndex { get; set; }
+
+        public bool? isLessonName(string html)
+        {
+            var indexName = html.IndexOf("<p>", startIndex);
+            var indexClassNumber = html.IndexOf("<p align=\"center\">", startIndex);
+
+            if (indexName == -1 && indexClassNumber == -1)
+            {
+                return null;
+            }
+            else if(indexName == -1 && indexClassNumber != -1)
+            {
+                return false;
+            }
+            else if (indexName != -1 && indexClassNumber == -1)
+            {
+                return true;
+            }
+
+            return null;
+        }
+    }
+}
+
+
+//  <td style="width:162px;height:19px;">
+//      <p>3.Английский язык</p>
+//  </td>
+//  <td style="width:47px;height:19px;">
+//      <p align="center">404</p>
+//      <p align="center">411</p>
+//  </td>
